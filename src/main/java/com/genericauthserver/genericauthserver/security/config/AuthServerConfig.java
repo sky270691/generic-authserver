235b089608d99de579dd0c5f7e139f6bde1c37a8
sky270691 <58143746+sky270691@users.noreply.github.com>
2020-06-25 22:53:48 +0800

    jwt authserver init with authorization code grant type

--- src/main/java/com/genericauthserver/genericauthserver/security/config/AuthServerConfig.java
+++ src/main/java/com/genericauthserver/genericauthserver/security/config/AuthServerConfig.java
@@ -1,24 +1,18 @@
 package com.genericauthserver.genericauthserver.security.config;
 
 import com.genericauthserver.genericauthserver.service.AppClientServiceImpl;
-import com.genericauthserver.genericauthserver.service.UserDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-
-import java.util.List;
-import java.util.Optional;
 
 @Configuration
 @EnableAuthorizationServer
@@ -56,7 +50,7 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
         endpoints.authenticationManager(authenticationManager)
-//                .userDetailsService(userDetailsService)
+                .userDetailsService(userDetailsService)
                 .tokenStore(tokenStore())
                 .accessTokenConverter(jwtAccessTokenConverter());
     }
