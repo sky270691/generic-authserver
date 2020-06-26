90eae9e4e2fcde4545cdef618d4807ae986d25e5
sky270691 <58143746+sky270691@users.noreply.github.com>
2020-06-26 16:50:38 +0800

    define postgresql datasource

--- src/main/java/com/genericauthserver/genericauthserver/controller/AuthCodeController.java
+++ src/main/java/com/genericauthserver/genericauthserver/controller/AuthCodeController.java
@@ -1,5 +1,8 @@
 package com.genericauthserver.genericauthserver.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,9 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthCodeController {
 
+    @Autowired
+    JavaMailSender javaMailSender;
+
     @GetMapping("/mycode")
     public String getCode(@RequestParam String code){
         System.out.println(code);
+        String to= "langi.risky@gmail.com";
+        String subject="Verification Code";
+        String text = "this is your verification code: "+code;
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(to);
+        mailMessage.setSubject(subject);
+        mailMessage.setText(text);
+        javaMailSender.send(mailMessage);
+
         return "your code is: "+code;
     }
 
