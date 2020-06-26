5cf7eae2d8965ce53d878857cb6009f600de99f0
sky270691 <58143746+sky270691@users.noreply.github.com>
2020-06-26 16:40:56 +0800

    define postgresql datasource

--- src/main/java/com/genericauthserver/genericauthserver/controller/AuthCodeController.java
+++ src/main/java/com/genericauthserver/genericauthserver/controller/AuthCodeController.java
@@ -3,14 +3,15 @@ package com.genericauthserver.genericauthserver.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class AuthCodeController {
 
     @GetMapping("/mycode")
     public String getCode(@RequestParam String code){
         System.out.println(code);
-        return "redirect:http://www.google.com";
+        return "your code is: "+code;
     }
 
 }
