b52447479346d58c59116b90b0edee9247d29fcf
sky270691 <58143746+sky270691@users.noreply.github.com>
2020-06-30 20:39:53 +0800

    define postgresql datasource

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,7 @@ FROM openjdk:13-alpine
 COPY . /app
 WORKDIR /app
 RUN chmod +x ./mvnw
+RUN mkdir /logs
+RUN touch /logs/authserver
 ENTRYPOINT ["./mvnw"]
 CMD ["spring-boot:run"]
--- src/main/java/com/genericauthserver/genericauthserver/controller/AuthCodeController.java
+++ src/main/java/com/genericauthserver/genericauthserver/controller/AuthCodeController.java
@@ -1,5 +1,7 @@
 package com.genericauthserver.genericauthserver.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -14,9 +16,12 @@ public class AuthCodeController {
     @Autowired
     private JavaMailSender javaMailSender;
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping("/mycode")
     public String getCode(@RequestParam String code){
         System.out.println(code);
+        logger.info("someone is trying to get code");
         String to= "langi.risky@gmail.com";
         String subject="Verification Code";
         String text = "this is your verification code: "+code;
