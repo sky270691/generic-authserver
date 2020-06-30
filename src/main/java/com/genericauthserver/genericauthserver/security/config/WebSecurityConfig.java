9603793ad95d9033dee4cf0308de8f4818c65451
sky270691 <58143746+sky270691@users.noreply.github.com>
2020-07-01 01:08:06 +0800

    define postgresql datasource

--- src/main/java/com/genericauthserver/genericauthserver/security/config/WebSecurityConfig.java
+++ src/main/java/com/genericauthserver/genericauthserver/security/config/WebSecurityConfig.java
@@ -36,7 +36,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin();
+        http.httpBasic();
 
         http.authorizeRequests().mvcMatchers("/mycode").permitAll().anyRequest().authenticated();
     }
