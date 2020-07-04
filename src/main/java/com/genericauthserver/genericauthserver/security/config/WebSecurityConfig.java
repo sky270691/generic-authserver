e08d8fcd841cea3848edcb88068ecc38dba8be0d
sky270691 <58143746+sky270691@users.noreply.github.com>
2020-07-04 23:38:22 +0800

    define postgresql datasource

--- src/main/java/com/genericauthserver/genericauthserver/security/config/WebSecurityConfig.java
+++ src/main/java/com/genericauthserver/genericauthserver/security/config/WebSecurityConfig.java
@@ -1,24 +1,12 @@
 package com.genericauthserver.genericauthserver.security.config;
 
-import com.genericauthserver.genericauthserver.service.UserDataService;
-import com.genericauthserver.genericauthserver.service.UserDataServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.provider.ClientDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
