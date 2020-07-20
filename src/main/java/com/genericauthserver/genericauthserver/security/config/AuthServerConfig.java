3ed72e7c84662d06e541f6d4f8a4ce29c0e52b39
sky270691 <58143746+sky270691@users.noreply.github.com>
2020-07-20 22:39:55 +0800

    first step login

--- src/main/java/com/genericauthserver/genericauthserver/security/config/AuthServerConfig.java
+++ src/main/java/com/genericauthserver/genericauthserver/security/config/AuthServerConfig.java
@@ -12,9 +12,12 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.util.List;
 
 @Configuration
 @EnableAuthorizationServer
@@ -56,10 +59,14 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+
+        TokenEnhancerChain enhancerChain = new TokenEnhancerChain();
+        enhancerChain.setTokenEnhancers(List.of(customTokenEnhancer(),jwtAccessTokenConverter()));
+
         endpoints.authenticationManager(authenticationManager)
-                .tokenStore(tokenStore())
-                .tokenEnhancer(customTokenEnhancer())
-                .accessTokenConverter(jwtAccessTokenConverter());
+//                .tokenStore(tokenStore())
+                .tokenEnhancer(enhancerChain);
+//                .accessTokenConverter(jwtAccessTokenConverter());
     }
 
 }
