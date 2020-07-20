646da618c87d947c5b606f2f018f9ca4e66c35e7
sky270691 <58143746+sky270691@users.noreply.github.com>
2020-07-20 22:46:13 +0800

    first step login

--- src/main/java/com/genericauthserver/genericauthserver/security/config/AuthServerConfig.java
+++ src/main/java/com/genericauthserver/genericauthserver/security/config/AuthServerConfig.java
@@ -5,15 +5,14 @@ import com.genericauthserver.genericauthserver.service.user.UserDataServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenEnhancer;
-import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
-import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.*;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
@@ -53,6 +52,15 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
     }
 
     @Bean
+    @Primary
+    public AuthorizationServerTokenServices tokenServices() {
+        DefaultTokenServices tokenServices = new DefaultTokenServices();
+
+        tokenServices.setTokenEnhancer(customTokenEnhancer());
+        return tokenServices;
+    }
+
+    @Bean
     public AppClientServiceImpl appClientService(){
         return new AppClientServiceImpl();
     }
@@ -65,7 +73,7 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
         endpoints.authenticationManager(authenticationManager)
 //                .tokenStore(tokenStore())
-                .tokenEnhancer(enhancerChain);
+                .tokenEnhancer(customTokenEnhancer());
 //                .accessTokenConverter(jwtAccessTokenConverter());
     }
 
