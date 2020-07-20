5d489803336b5f68068fcd33fde1c055875d909b
sky270691 <58143746+sky270691@users.noreply.github.com>
2020-07-20 15:59:51 +0800

    first step login

--- src/main/java/com/genericauthserver/genericauthserver/security/config/WebSecurityConfig.java
+++ src/main/java/com/genericauthserver/genericauthserver/security/config/WebSecurityConfig.java
@@ -30,7 +30,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().mvcMatchers("/api/v1/login*/*").permitAll();
+        http.authorizeRequests().mvcMatchers("/api/v1/login").permitAll();
         http.httpBasic();
         http.authorizeRequests().mvcMatchers("/mycode").authenticated();
     }
