9d57fd898fa7c83db45bfbfe2ef408956be8ac12
sky270691 <58143746+sky270691@users.noreply.github.com>
2020-07-21 14:20:29 +0800

    register user phase 1

--- src/main/java/com/genericauthserver/exception/CustomGlobalExceptionHandler.java
+++ src/main/java/com/genericauthserver/exception/CustomGlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -15,6 +16,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@ControllerAdvice
 public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     private final String DATE_TIME_FORMAT = "yyyy-MM-dd HH-mm-ss";
