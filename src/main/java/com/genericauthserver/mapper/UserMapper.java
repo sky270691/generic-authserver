79d972be7a7bd55ab7bc4f6476b40fb7947c9b09
sky270691 <58143746+sky270691@users.noreply.github.com>
2020-07-21 13:04:25 +0800

    register user phase 1

--- src/main/java/com/genericauthserver/mapper/UserMapper.java
+++ src/main/java/com/genericauthserver/mapper/UserMapper.java
@@ -8,7 +8,6 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-@Component
 public class UserMapper {
 
     public User convertUserRegisterUpdateDtoToUserEntity(UserRegisterUpdateDto dto, PasswordEncoder encoder){
