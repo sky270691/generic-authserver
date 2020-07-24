afd96a53ac8bfb51073a1f8329c11c5983d456b6
sky270691 <58143746+sky270691@users.noreply.github.com>
2020-07-24 23:27:54 +0800

    Add Sex to User entity

--- src/main/java/com/genericauthserver/GenericAuthServerApplication.java
+++ src/main/java/com/genericauthserver/GenericAuthServerApplication.java
@@ -2,8 +2,10 @@ package com.genericauthserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class GenericAuthServerApplication {
 
     public static void main(String[] args) {
